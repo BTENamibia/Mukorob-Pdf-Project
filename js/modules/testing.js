17bfaf6576b9e06e6c4621dda30036b64d95f3ea
Bradz Isaak <info.btenamibia@gmail.com>
2026-09-16 00:12:18 +0200

    v0.7.1: update browser test hook version

--- js/modules/testing.js
+++ js/modules/testing.js
@@ -1,7 +1,9 @@
-/* v0.7 browser-test hooks. These are inert in normal use and make key flows
+/* v0.7.1 browser-test hooks. These are inert in normal use and make key flows
    deterministic for Playwright/Cypress without exposing credentials. */
 window.MukorobTestHooks={
-  version:'0.7.0',
+  version:'0.7.1',
   getState:()=>({authenticated:!!window.MukorobApp?.state?.auth?.user,documentOpen:!!window.MukorobApp?.state?.pdfDoc,page:window.MukorobApp?.state?.currentPage||0}),
-  getAnnotationCount:()=>window.MukorobApp?.state?.annotations?.length||0
+  getAnnotationCount:()=>window.MukorobApp?.state?.annotations?.length||0,
+  hasMigrationUI:()=>Boolean(document.querySelector('#mkMigrationPanel')),
+  hasFirstRunRestore:()=>Boolean(document.querySelector('#mkFirstRunRestore'))
 };
