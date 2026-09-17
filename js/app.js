e4cfe8e9cc8e27798c6cb4b940d8321001b4bcd3
Bradz Isaak <info.btenamibia@gmail.com>
2026-09-17 23:35:47 +0200

    fix: remove unstable v0.7.1 corrective runtime layer

--- js/app.js
+++ js/app.js
@@ -5,5 +5,4 @@ import './modules/organizer.js';
 import './modules/autosave.js';
 import './modules/data-migration.js';
 import './modules/testing.js';
-import './modules/v071-fixes.js';
 import './modules/v071-brand-auth.js';
